9d58c9bbd98bb56f27c1ecbb3e35f3d81d2b81ed
yomna raouf <yomnaraouf99@gmail.com>
2020-10-11 22:14:18 -0700

    add todos to a todo list

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredTodo, setEnteredTodo] = useState('');
+  const [Todos, setTodos] = useState([]);
+
+  const InputHandler = (enteredText) => {
+    setEnteredTodo(enteredText)
+  }
+
+  const addTodoHandler = () => {
+    setTodos(Todos => [...Todos, enteredTodo]);
+    console.log(Todos);
+    setEnteredTodo('');
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer} >
         <TextInput
           placeholder="Your Todos"
           style={styles.input}
+          value={enteredTodo}
+          onChangeText={InputHandler}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addTodoHandler} />
       </View>
       <View>
-
+        {Todos.map((Todo, index) => <Text key={index}>{Todo}</Text>)}
       </View>
     </View>
   );
