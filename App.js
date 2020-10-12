60783746a7061dd0ab1272d87744e0782e8b252c
yomna raouf <yomnaraouf99@gmail.com>
2020-10-11 20:48:10 -0700

    Input field and add button added

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text >My First Mobile App! wala!! I LOVE IT AWESOME LET'S GOOOOOO!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer} >
+        <TextInput
+          placeholder="Your Todos"
+          style={styles.input}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10
+  screen: {
+    padding: 50
   },
-   
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginBottom: 8,
+    padding: 10,
+    flexGrow: .8
+  }
 });
+
+
+
+
+
+
+
+
+
